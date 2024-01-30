f706fd58aacfd646958a06d7444a4969cdb1dd25
tegar97 <eategar@gmail.com>
2024-01-30 18:22:02 +0700

    finish setup drawer and fragment

---
+++ app/src/main/java/com/tegar/tourismapp/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.tegar.tourismapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import com.tegar.tourismapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.appBarMain.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener(this)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, HomeFragment())
+                .commit()
+            supportActionBar?.title = getString(R.string.app_name)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment : Fragment? = null
+        var title = getString(R.string.app_name)
+
+        when(item.itemId){
+            R.id.nav_home -> {
+                fragment = HomeFragment()
+                title = "Tourism App"
+            }
+            R.id.nav_favorite ->{
+                fragment = FavoriteFragment()
+                title = "Favorite"
+            }
+            R.id.nav_map -> {
+                Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit()
+        }
+        supportActionBar?.title = title
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
